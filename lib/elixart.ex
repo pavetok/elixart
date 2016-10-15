35c84ff795dde0e9ae7a1785ca85ec3ad4c43de6
pavetok <p.vetokhin@gmail.com>
2016-10-15 17:09:49 +0300

    intermediate commit

--- lib/elixart.ex
+++ lib/elixart.ex
@@ -7,4 +7,16 @@ defmodule Elixart do
   def capitalize(str) do
     Enum.map_join(String.split(str) , " ", fn word -> String.capitalize(word) end)
   end
+
+  def calculate(job: job,
+                wc: wc,
+                hc: hc,
+                init: init) do
+    send self(), {:result, job.(init) + hc}
+    receive do
+      {:result, value} -> value
+    after
+      500 -> raise "Something went wrong."
+    end
+  end
 end
--- test/elixart_test.exs
+++ test/elixart_test.exs
@@ -3,16 +3,23 @@ defmodule ElixartTest do
   import Elixart
   doctest Elixart
 
-  test "the truth" do
-    assert 1 + 1 == 2
-  end
-
-  test "hello" do
-    assert hello == "Hello Worl"
-  end
-
   test "capitalize" do
     assert capitalize("name") == "Name"
     assert capitalize("name second") == "Name Second"
+  end
+
+  test "calculate increment" do
+    inc = fn(x) -> x + 1 end
+    assert calculate(job: inc, wc: 1, hc: 0, init: 0) == 1
+    assert calculate(job: inc, wc: 1, hc: 1, init: 0) == 2
+    assert calculate(job: inc, wc: 1, hc: 2, init: 0) == 3
+    assert calculate(job: inc, wc: 1, hc: 2, init: 1) == 4
+  end
+
+  test "calculate double" do
+    double = fn(x) -> x * 2 end
+    assert calculate(job: double, wc: 1, hc: 0, init: 0) == 0
+    assert calculate(job: double, wc: 1, hc: 0, init: 1) == 2
+    assert calculate(job: double, wc: 1, hc: 1, init: 1) == 4
   end
 end
