8f2640ec24dce8e701295dbccd9608dea13b135e
pavetok <p.vetokhin@gmail.com>
2016-10-16 10:05:07 +0300

    process ring

--- lib/elixart.ex
+++ lib/elixart.ex
@@ -8,11 +8,59 @@ defmodule Elixart do
     Enum.map_join(String.split(str) , " ", fn word -> String.capitalize(word) end)
   end
 
-  def calculate(job: job,
-                wc: wc,
-                hc: hc,
-                init: init) do
-    send self(), {:result, job.(init) + hc}
+  def pong do
+    receive do
+      {:ping, from} -> send from, :pong
+    end
+  end
+
+  def stateful(address) do
+    receive do
+      {:address, target} ->
+        stateful(target)
+      :ping ->
+        send address, :pong
+        stateful(address)
+    end
+  end
+
+  def worker(initiator, job, next_worker) do
+    receive do
+      {:next, pid} ->
+        worker(initiator, job, pid)
+
+      {:calc, 0, acc} ->
+        send initiator, {:result, job.(acc)}
+        Process.exit(next_worker, :normal)
+        Process.exit(self(), :normal)
+
+      {:calc, rest_hops, acc} ->
+        send next_worker, {:calc, rest_hops - 1, job.(acc)}
+        worker(initiator, job, next_worker)
+
+      _ -> IO.puts "i am from worker"
+    end
+  end
+
+  defp create_ring(initiator, job, size) do
+    entry_point = spawn_link(Elixart, :worker, [initiator, job, nil])
+    last_worker = create_workers(initiator, job, size - 1, entry_point)
+    send entry_point, {:next, last_worker}
+    entry_point
+  end
+
+  defp create_workers(_, _, count, last_worker) when count == 0 do
+    last_worker
+  end
+
+  defp create_workers(initiator, job, count, next_worker) do
+    new_worker = spawn_link(Elixart, :worker, [initiator, job, next_worker])
+    create_workers(initiator, job, count - 1, new_worker)
+  end
+
+  def calculate(job: job, wc: worker_count, hc: hop_count, init: init_value) do
+    entry_point = create_ring(self(), job, worker_count)
+    send entry_point, {:calc, hop_count, init_value}
     receive do
       {:result, value} -> value
     after
--- test/elixart_test.exs
+++ test/elixart_test.exs
@@ -8,18 +8,43 @@ defmodule ElixartTest do
     assert capitalize("name second") == "Name Second"
   end
 
+  test "ping pong" do
+    pid = spawn_link(Elixart, :pong, [])
+    send pid, {:ping, self()}
+    assert_receive :pong
+  end
+
+  test "stateful ping pong" do
+    pid = spawn_link(Elixart, :stateful, [nil])
+    send pid, {:address, self()}
+    # first
+    send pid, :ping
+    assert_receive :pong
+    # second
+    send pid, :ping
+    assert_receive :pong
+  end
+
   test "calculate increment" do
     inc = fn(x) -> x + 1 end
     assert calculate(job: inc, wc: 1, hc: 0, init: 0) == 1
     assert calculate(job: inc, wc: 1, hc: 1, init: 0) == 2
     assert calculate(job: inc, wc: 1, hc: 2, init: 0) == 3
     assert calculate(job: inc, wc: 1, hc: 2, init: 1) == 4
+    assert calculate(job: inc, wc: 10, hc: 20, init: 10) == 31
   end
 
   test "calculate double" do
     double = fn(x) -> x * 2 end
     assert calculate(job: double, wc: 1, hc: 0, init: 0) == 0
+    assert calculate(job: double, wc: 5, hc: 5, init: 0) == 0
     assert calculate(job: double, wc: 1, hc: 0, init: 1) == 2
     assert calculate(job: double, wc: 1, hc: 1, init: 1) == 4
+    assert calculate(job: double, wc: 2, hc: 0, init: 1) == 2
+    assert calculate(job: double, wc: 2, hc: 1, init: 1) == 4
+    assert calculate(job: double, wc: 2, hc: 2, init: 1) == 8
+    assert calculate(job: double, wc: 2, hc: 3, init: 1) == 16
   end
+
+
 end
